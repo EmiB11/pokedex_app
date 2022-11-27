0601003475b1a57ab3e60b2a1d25af2896e80da2
Emiliano Barberis <emibarberis26@gmail.com>
2022-11-26 22:46:23 -0300

    prod con jersey

--- src/main/java/com/pokemon/backend/config/JerseyConfig.java
+++ src/main/java/com/pokemon/backend/config/JerseyConfig.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.pokemon.backend.config;
+/*package com.pokemon.backend.config;
 
 import javax.ws.rs.ApplicationPath;
 
@@ -11,8 +11,10 @@ import org.springframework.stereotype.Component;
 
 @ApplicationPath("/")
 @Component
+
 public class JerseyConfig extends ResourceConfig {
    public JerseyConfig(){
     this.packages("com.pokemon.backend.controller");
    }
 }
+*/
